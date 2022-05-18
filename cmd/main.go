c39c2774638cbc929d2a919a2ae1cb85fd40786b
Daniel Rammer <hamersaw@protonmail.com>
2022-05-18 14:15:23 -0500

    creating and patching a flyteworkflow to update node status
    Signed-off-by: Daniel Rammer <hamersaw@protonmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,154 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+
+	"github.com/flyteorg/flytepropeller/pkg/apis/flyteworkflow"
+	"github.com/flyteorg/flytepropeller/pkg/apis/flyteworkflow/v1alpha1"
+	clientset "github.com/flyteorg/flytepropeller/pkg/client/clientset/versioned"
+	//v1alpha12 "github.com/flyteorg/flytepropeller/pkg/client/clientset/versioned/typed/flyteworkflow/v1alpha1"
+	"github.com/flyteorg/flytepropeller/pkg/controller/config"
+	"github.com/flyteorg/flytepropeller/pkg/utils"
+
+	apiextensionsclientset "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	types "k8s.io/apimachinery/pkg/types"
 )
 
+const (
+	namespace = "flytesnacks-development"
+	nodeCount = 10
+)
+
+type NodeStatusPatch struct {
+	Op    string               `json:"op"`
+	Path  string               `json:"path"`
+	Value *v1alpha1.NodeStatus `json:"value"`
+}
+
+/*type NodeStatus struct {
+	MutableStruct
+	Phase                NodePhase     `json:"phase,omitempty"`
+	QueuedAt             *metav1.Time  `json:"queuedAt,omitempty"`
+	StartedAt            *metav1.Time  `json:"startedAt,omitempty"`
+	StoppedAt            *metav1.Time  `json:"stoppedAt,omitempty"`
+	LastUpdatedAt        *metav1.Time  `json:"lastUpdatedAt,omitempty"`
+	LastAttemptStartedAt *metav1.Time  `json:"laStartedAt,omitempty"`
+	Message              string        `json:"message,omitempty"`
+	DataDir              DataReference `json:"-"`
+	OutputDir            DataReference `json:"-"`
+	Attempts             uint32        `json:"attempts,omitempty"`
+	SystemFailures       uint32        `json:"systemFailures,omitempty"`
+	Cached               bool          `json:"cached,omitempty"`
+
+	// This is useful only for branch nodes. If this is set, then it can be used to determine if execution can proceed
+	ParentNode    *NodeID                  `json:"parentNode,omitempty"`
+	ParentTask    *TaskExecutionIdentifier `json:"-"`
+	BranchStatus  *BranchNodeStatus        `json:"branchStatus,omitempty"`
+	SubNodeStatus map[NodeID]*NodeStatus   `json:"subNodeStatus,omitempty"`
+	// We can store the outputs at this layer
+
+	// TODO not used delete
+	WorkflowNodeStatus *WorkflowNodeStatus `json:"workflowNodeStatus,omitempty"`
+
+	TaskNodeStatus    *TaskNodeStatus    `json:",omitempty"`
+	DynamicNodeStatus *DynamicNodeStatus `json:"dynamicNodeStatus,omitempty"`
+	// In case of Failing/Failed Phase, an execution error can be optionally associated with the Node
+	Error *ExecutionError `json:"error,omitempty"`
+
+	// Not Persisted
+	DataReferenceConstructor storage.ReferenceConstructor `json:"-"`
+}*/
+
 func main() {
-	fmt.Println("Hello World!")
+	ctx := context.Background()
+
+	// initialize kube client 
+	cfg := &config.Config{
+		KubeConfigPath: "$HOME/.kube/config",
+	}
+
+	_, kubecfg, err := utils.GetKubeConfig(ctx, cfg)
+	if err != nil {
+		fmt.Printf("failed to get kube config with err '%v'\n", err)
+		return
+	}
+
+	flyteworkflowClient, err := clientset.NewForConfig(kubecfg)
+	if err != nil {
+		fmt.Printf("failed to initialize flyteworkflow clientset with err '%v'\n", err)
+		return
+	}
+
+	// create flyteworkflow CRD if it does not exist
+	apiextensionsClient, err := apiextensionsclientset.NewForConfig(kubecfg)
+	if err != nil {
+		fmt.Printf("failed to intiialize apiextensions clientset with err '%v'\n", err)
+		return
+	}
+
+	_, err = apiextensionsClient.ApiextensionsV1().CustomResourceDefinitions().Create(ctx, &flyteworkflow.CRD, metav1.CreateOptions{})
+	if err != nil {
+		if !apierrors.IsAlreadyExists(err) {
+			fmt.Printf("failed to create FlyteWorkflow CRD with err '%v'\n", err)
+			return
+		}
+	}
+
+	// create workflow
+	nodeStatuses := make(map[v1alpha1.NodeID]*v1alpha1.NodeStatus)
+	for i:=0; i<nodeCount; i++ {
+		nodeStatuses[fmt.Sprintf("node-%d", i)] = &v1alpha1.NodeStatus{
+			Phase: v1alpha1.NodePhaseNotYetStarted,
+		}
+	}
+	//fmt.Printf("%+v\n", workflow)
+
+	workflow := &v1alpha1.FlyteWorkflow{
+		TypeMeta: metav1.TypeMeta{
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "foo",
+			Namespace: namespace,
+		},
+		Status: v1alpha1.WorkflowStatus{
+			NodeStatus: nodeStatuses,
+		},
+	}
+
+
+	workflow, err = flyteworkflowClient.FlyteworkflowV1alpha1().FlyteWorkflows(namespace).Create(ctx, workflow, metav1.CreateOptions{})
+	if err != nil {
+		fmt.Printf("failed to create FlyteWorkflow CRD with err '%v'\n", err)
+		return
+	}
+
+	// patch workflow
+	for i:=0; i<nodeCount; i++ {
+		nodeId := fmt.Sprintf("node-%d", i)
+		status := workflow.Status.NodeStatus[nodeId]
+		status.Phase = v1alpha1.NodePhaseRunning
+		
+		nodeStatusPatch := []NodeStatusPatch{{
+			Op:   "replace",
+			Path: fmt.Sprintf("/status/nodeStatus/node-%d", i),
+			Value: status,
+		}}
+
+		patchBytes, err := json.Marshal(nodeStatusPatch)
+		if err != nil {
+			fmt.Printf("failed to marshal FlyteWorkflow CRD patch err '%v'\n", err)
+			continue
+		}
+
+		workflow, err = flyteworkflowClient.FlyteworkflowV1alpha1().FlyteWorkflows(namespace).Patch(ctx, workflow.ObjectMeta.Name, types.JSONPatchType, patchBytes, metav1.PatchOptions{})
+		if err != nil {
+			fmt.Printf("failed to patch FlyteWorkflow CRD with err '%v'\n", err)
+			return
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,87 @@ module github.com/hamersaw/k8s-patch-benchmark
 
 go 1.18
 
-require (
-)
+require github.com/flyteorg/flytepropeller v1.1.3
 
+require (
+	cloud.google.com/go v0.78.0 // indirect
+	cloud.google.com/go/storage v1.12.0 // indirect
+	github.com/Azure/azure-sdk-for-go v62.3.0+incompatible // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v0.21.1 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/internal v0.8.3 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/storage/azblob v0.3.0 // indirect
+	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
+	github.com/Azure/go-autorest/autorest v0.11.17 // indirect
+	github.com/Azure/go-autorest/autorest/adal v0.9.10 // indirect
+	github.com/Azure/go-autorest/autorest/date v0.3.0 // indirect
+	github.com/Azure/go-autorest/logger v0.2.0 // indirect
+	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
+	github.com/aws/aws-sdk-go v1.37.3 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/coocood/freecache v1.1.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fatih/color v1.10.0 // indirect
+	github.com/flyteorg/flyteidl v1.1.0 // indirect
+	github.com/flyteorg/flytestdlib v1.0.0 // indirect
+	github.com/flyteorg/stow v0.3.3 // indirect
+	github.com/form3tech-oss/jwt-go v3.2.2+incompatible // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-logr/logr v0.4.0 // indirect
+	github.com/gofrs/uuid v4.2.0+incompatible // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/google/gofuzz v1.2.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.0.5 // indirect
+	github.com/googleapis/gnostic v0.5.1 // indirect
+	github.com/imdario/mergo v0.3.11 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/jstemmer/go-junit-report v0.9.1 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.2-0.20181231171920-c182affec369 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/ncw/swift v1.0.53 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.10.0 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.18.0 // indirect
+	github.com/prometheus/procfs v0.6.0 // indirect
+	github.com/sirupsen/logrus v1.7.0 // indirect
+	github.com/spf13/cobra v1.1.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	go.opencensus.io v0.22.6 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/lint v0.0.0-20201208152925-83fdc39ff7b5 // indirect
+	golang.org/x/mod v0.6.0-dev.0.20220106191415-9b9b3d81d5e3 // indirect
+	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
+	golang.org/x/oauth2 v0.0.0-20210220000619-9bb904979d93 // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
+	golang.org/x/tools v0.1.10 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/api v0.40.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20210222152913-aa3ee6e6a81c // indirect
+	google.golang.org/grpc v1.36.0 // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	k8s.io/api v0.20.2 // indirect
+	k8s.io/apiextensions-apiserver v0.20.1 // indirect
+	k8s.io/apimachinery v0.20.2 // indirect
+	k8s.io/client-go v0.20.2 // indirect
+	k8s.io/klog/v2 v2.5.0 // indirect
+	k8s.io/kube-openapi v0.0.0-20201113171705-d219536bb9fd // indirect
+	k8s.io/utils v0.0.0-20210111153108-fddb29f9d009 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.0.3 // indirect
+	sigs.k8s.io/yaml v1.2.0 // indirect
+)
